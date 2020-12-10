Fix Crossover_uniform fix index of the chage in case there is chromossomes with different length;adds a numba function for fronts calculation _fronts time/exec for 100 geneerations is 197 sec

02_implementacao/capacidade.py
@@ -806,7 +806,9 @@ class Planning():
 
         # 4)Front Classification
         # a0=np.sum(copy.deepcopy(pop.objectives_raw))
-        pop.fronts=AlgNsga2._fronts(pop.objectives_raw,self.num_fronts)
+        # pop.fronts=AlgNsga2._fronts(pop.objectives_raw,self.num_fronts)
+        pop.fronts=AlgNsga2._fronts_numba(pop.objectives_raw,self.num_fronts)
+   
         # a1=np.sum(pop.objectives_raw)
         # if (a1-a0)!=0:
         #     raise Exception('Mutation is affecting values, consider making a deepcopy.')
@@ -978,7 +980,7 @@ class Planning():
         # Parameters
 
         # Number of executions
-        n_exec=50
+        n_exec=4
         n_exec_ite=range(0,n_exec)
 
         # Variation 1
@@ -1013,7 +1015,10 @@ class Planning():
             results[(v_i[0],v_i[1],v_i[2],v_i[3],v_i[4])]=results_append
             tf=time.perf_counter()
             delta_t=tf-t0
-            print("Tempo ",delta_t)
+            try:
+                print("Total time ",delta_t,"Time/execution ",delta_t/ng)
+            except Exception:
+                pass
             times.append(delta_t)
 
         root_path = "C:\\Users\\Debora\\Documents\\01_UFU_local\\01_comp_evolutiva\\05_trabalho3\\01_dados\\01_raw\\"
@@ -1044,29 +1049,29 @@ class Planning():
         """
         num_exec=1
         num_chromossomes=100
-        num_geracoes=2
+        num_geracoes=100
         n_tour=2
         pcross=0.6
         # Parameters for the mutation operator (pmutp,pposb,pnegb,pswap)
         pmut=(0.04,0.61,0.77,0.47)
 
-        results=Planning().main(num_chromossomes,num_geracoes,n_tour,pcross,pmut)
+        # results=Planning().main(num_chromossomes,num_geracoes,n_tour,pcross,pmut)
         # cProfile.runctx("results,num_exec=Planning().main(num_exec,num_chromossomes,num_geracoes,n_tour,pcross,pmut)", globals(), locals())
 
-        # pr = cProfile.Profile()
-        # pr.enable()
-        # pr.runctx("results,num_exec=Planning().main(num_exec,num_chromossomes,num_geracoes,n_tour,pcross,pmut)", globals(), locals())
-        # pr.disable()
-        # s = io.StringIO()
-        # sortby = SortKey.CUMULATIVE
-        # ps = pstats.Stats(pr, stream=s).sort_stats("cumtime")
-        # root_path = "C:\\Users\\Debora\\Documents\\01_UFU_local\\01_comp_evolutiva\\05_trabalho3\\01_dados\\01_raw\\"
-        # file_name = "cprofile.txt"
-        # path = root_path + file_name
-        # ps.print_stats()
-        # with open(path, 'w+') as f:
-        #     f.write(s.getvalue())
+        pr = cProfile.Profile()
+        pr.enable()
+        pr.runctx("results=Planning().main(num_chromossomes,num_geracoes,n_tour,pcross,pmut)", globals(), locals())
+        pr.disable()
+        s = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=s).sort_stats("cumtime")
+        root_path = "C:\\Users\\Debora\\Documents\\01_UFU_local\\01_comp_evolutiva\\05_trabalho3\\01_dados\\01_raw\\"
+        file_name = "cprofile.txt"
+        path = root_path + file_name
+        ps.print_stats()
+        with open(path, 'w+') as f:
+            f.write(s.getvalue())
 
 if __name__=="__main__":
-    # Planning.run_cprofile()
-    Planning.run_parallel()
+    Planning.run_cprofile()
+    # Planning.run_parallel()

02_implementacao/test_1.py
@@ -1,6 +1,4 @@
 import numpy as np
-a=np.array([0.0,
-0.0,
-(2.1, 3.1, 4.65)])
-print(a[2])
-print(a[2][0])
+
+fronts=np.empty(dtype=int,shape=(1,0))
+print(fronts)

02_implementacao/tets.py
@@ -16,83 +16,13 @@
 
 import numpy as np
 from functools import partial
+import pandas as pd
 # from collections import defaultdict
 # from dateutil import relativedelta
 # import datetime
-from numba import cuda
+ix_to_delete=np.array([-10])
+ix_falta_classificar=np.arange(1,2)
+ix_to_delete=np.append(ix_to_delete,np.where(ix_falta_classificar==0))
+ix_to_delete=np.delete(ix_to_delete,0)
 
-# a=defaultdict(partial)
-
-# b=np.zeros(shape=(1,2))
-# c=np.ones(shape=(1,2))
-# a[0]=(b,c)
-
-# print("d")
-# # d1=datetime.datetime(2018,10,5)
-# # d2=datetime.datetime(2019,11,6)
-
-# # r = relativedelta.relativedelta(d2, d1)
-# # print(r.months)
-
-# # print(12*r.years)
-# target_0=[6.2,6.2,9.3,9.3,12.4,12.4,15.5,21.7,21.7,24.8,21.7,24.8,27.9,21.7,24.8,24.8,24.8,27.9,27.9,27.9,31,31,34.1,34.1,27.9,27.9,27.9,27.9,34.1,34.1,31,31,21.7,15.5,6.2,0]
-# target_1=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2,6.2]
-# target_2=[0,4.9,9.8,9.8,9.8,9.8,19.6,19.6,14.7,19.6,19.6,19.6,14.7,19.6,19.6,14.7,14.7,19.6,19.6,9.8,19.6,19.6,19.6,19.6,24.5,34.3,24.5,29.4,39.2,39.2,29.4,19.6,19.6,14.7,4.9,0]
-# target_3=[22,27.5,27.5,27.5,27.5,33,33,27.5,27.5,27.5,38.5,33,33,33,33,33,27.5,33,33,33,38.5,33,38.5,33,33,33,33,44,33,33,33,33,22,11,11,5.5]
-# # a=np.array(target_0,target_1,target_2,target_3)
-# a=np.column_stack([target_0,target_1,target_2,target_3])
-
-# print(a.shape)
-
-# a=np.ones(shape=(10,4))*9
-# b=np.ones(shape=(10,4))*5
-# f=np.ones(shape=(10,15))*7
-mask_valid=np.zeros(shape=(10,15),dtype=bool)
-mask_valid[0]=True
-print(mask_valid[0,2:])
-print(mask_valid[0,2:].shape)
-
-
-# # f[f<=2]=1
-# # print(f)
-# mask_valid[:,0:4]=np.ones(shape=(10,4),dtype=bool)
-# print(f)
-# for i in range(0,9,2):
-#     mask=np.random.randint(2,size=(1,4))
-#     mask_invert=mask^1
-#     f[i][mask_valid[i]]=a[i]*mask+b[i]*mask_invert
-#     f[mask_valid[i]]=a[i]*mask+b[i]*mask_invert
-#     f[mask_valid[i+1]]=b[i]*mask+a[i]*mask_invert
-#     print(f)
-
-
-# chromossome=np.random.randint(0,4,size=15)
-# range_max=4
-# pmutp=0.3
-# mask=np.random.randint(0,100,size=chromossome.shape)
-# ix_mut=np.where(mask<=pmutp*100)
-# print(chromossome)
-# chromossome[ix_mut]=chromossome[ix_mut]+1
-# print(chromossome)
-# mask=np.random.randint(0,100,size=chromossome.shape)
-# ix_mut=np.where(mask<=pmutp*100)
-# print(chromossome)
-# chromossome[ix_mut]=chromossome[ix_mut]-1
-# print(chromossome)
-
-# num_genes=20
-# batches=np.ones(shape=(20,num_genes),dtype=int)
-# batches_new=np.zeros(shape=(60,num_genes),dtype=int)
-# batches=np.vstack((batches,batches_new))
-# print(batches)
-# print(batches.shape)
-# a=[i for i in range(0,10)]
-# # print(len(a))
-# # b=[5 for i in range(0,5)]
-# # for i in range(0,len(b)):
-# #     a.append(b[i])
-# # print(len(a))
-# # print(a)
-# b=np.array([0,5,7])
-# # print(a[list(b)])
-# print(list(map(a.__getitem__,list(b))))
+print(ix_to_delete)