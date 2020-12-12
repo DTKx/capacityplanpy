20a6642b6778005ad904d9f5c6c000ecd8154d97
DTKx <63344441+DTKx@users.noreply.github.com>
2020-12-12 14:44:57 -0300

    Added tr_demand to class variables

--- 02_implementacao/analysis.py
+++ 02_implementacao/analysis.py
@@ -28,21 +28,31 @@ infile = open(path,'rb')
 ds_ind= pickle.load(infile)
 infile.close()
 
-for key,value in ds_exec.items():
-    print(key)
-    print(value)
-    print(value[0])
-df_exec=pd.DataFrame.from_dict(ds_exec,orient="index")
-best_pareto=pd.iloc[ix_max,1]
-best_pareto=df.at[ix_max,1]
-# df_exec.reset_index()
-# ds_exec[(0,0)][0]
+# for key,value in ds_exec.items():
+#     print(key)
+#     print(value)
+#     print(value[0])
+# df_exec=pd.DataFrame.from_dict(ds_exec,orient="index")
+# best_pareto=pd.iloc[ix_max,1]
+# best_pareto=df.at[ix_max,1]
+# # df_exec.reset_index()
+# # ds_exec[(0,0)][0]
 
 values_ind=[]
+# values_ind_test=[]
 for key,value in ds_ind.items():
-    values_ind.append(value)
+    # values_ind.append(value)
+    values_ind.append(value[0])
+    values_ind.append(value[1])
     # print(key)
     # print(value)
-    # print(value[1])
+    # print(value[0])
 
-df_ind=pd.DataFrame(values_ind[0])
+df_ind=pd.DataFrame(values_ind)
+# df_ind_2=pd.DataFrame(values_ind_test)
+file_name='data_test.csv'
+path=root_path_data+file_name
+headers=["Num_exec","Solution","Total throughput [kg]", "Max total backlog [kg]", "Mean total backlog [kg]","std dev total backlog [kg]", "Median total backlog [kg]","Min total backlog [kg]", "P(total backlog ≤ 0 kg)","Max total inventory deficit [kg]", "Mean total inventory deficit [kg]","std dev inventory deficit [kg]", "Median total inventory deficit [kg]", "Min total inventory deficit [kg]","Batches [un]","Product label","Start of USP [date]","End of DSP [date]"]
+df_ind.to_csv(path,header=headers)
+
+# df_ind.to_csv(path)
